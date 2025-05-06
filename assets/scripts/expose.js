9d18dd2ac32ad9bf85aa20a21009b2ee41a02515
Arul Mathur <arulster17@gmail.com>
2025-05-06 03:45:10 -0700

    finished expose

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,5 +3,56 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  var hornChosen = false;
+  let curHorn = 'select';
+  let hornSelect = document.getElementById('horn-select');
+  let hornImage = document.querySelector('#expose img');
+  let hornAudio = document.querySelector('#expose audio');
+
+
+  let volumeSelect = document.getElementById('volume');
+  let volumeImage = document.querySelector('#volume-controls img');
+
+  let playButton = document.querySelector('#expose button');
+  
+
+  
+  hornSelect.addEventListener('input', function () {
+    hornChosen = true;
+    curHorn = hornSelect.value;
+    let curImage = '/assets/images/' + curHorn + '.svg';
+    let curAudio = '/assets/audio/' + curHorn + '.mp3';
+
+    
+    hornImage.src = curImage;
+    hornAudio.src = curAudio;
+  });
+
+  volumeSelect.addEventListener('input', function () {
+    let curVolume = volumeSelect.value;
+    if (curVolume == 0) {
+      volumeImage.src = '/assets/icons/volume-level-0.svg';
+    }
+    else if (curVolume < 33) {
+      volumeImage.src = '/assets/icons/volume-level-1.svg';
+    }
+    else if (curVolume < 67) {
+      volumeImage.src = '/assets/icons/volume-level-2.svg';
+    }
+    else {
+      volumeImage.src = '/assets/icons/volume-level-3.svg';
+    }
+    hornAudio.volume = curVolume/100;
+    
+  });
+
+  playButton.addEventListener('click', function () {
+    if (!hornChosen) return;
+    hornAudio.play();
+    if(curHorn == 'party-horn') {
+      const jsConfetti = new JSConfetti();
+      jsConfetti.addConfetti();
+    }
+  });
+
 }
