09249178bb2cae81199f518caadaed5ad816b075
Arul Mathur <arulster17@gmail.com>
2025-05-06 18:53:42 -0700

    done with explore

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -2,6 +2,46 @@
 
 window.addEventListener('DOMContentLoaded', init);
 
+
 function init() {
-  // TODO
+
+  let playButton = document.querySelector('#explore button');
+  let voiceSelect = document.getElementById('voice-select');
+  let textInput = document.getElementById('text-to-speak');
+  let faceImage = document.querySelector('#explore img');
+  let synth = window.speechSynthesis;
+  let voices = synth.getVoices();
+
+  voiceSelect.addEventListener('mousedown', function() {
+    voiceSelect.innerHTML = '';
+    voices = synth.getVoices();
+    for (let i = 0; i < voices.length; i++) {
+      let newOption = document.createElement("option");
+      newOption.text = `${voices[i].name}`;
+      voiceSelect.append(newOption);
+    }
+    console.log("hi");
+
+  });
+
+  playButton.addEventListener('click', function() {
+    let textToSpeak = textInput.value;
+    let utterance = new SpeechSynthesisUtterance(textToSpeak);
+    for (let i = 0; i < voices.length; i++) {
+      if(voices[i].name == voiceSelect.value) {
+        utterance.voice = voices[i];
+      }
+    }
+    utterance.addEventListener('end', () => {
+      console.log("Speech has finished.");
+      faceImage.src = 'assets/images/smiling.png';
+      // You can trigger any follow-up action here
+    });
+    faceImage.src = 'assets/images/smiling-open.png';
+    synth.speak(utterance);
+  });
+
+ 
+
+
 }
